35f6d0c4a7b1ba623700ab145934605a33c260b0
cambo602 <54902084+cambo602@users.noreply.github.com>
2020-11-18 13:28:46 -0500

    Finished lesson 1.5

--- Lessons/src/Lessons_5/D_UserUserInputWithLoopsSolution.java
+++ Lessons/src/Lessons_5/D_UserUserInputWithLoopsSolution.java
@@ -1,5 +1,27 @@
 package Lessons_5;
+/* <----- Click this to Expand for Instructions
+ * 
+ * Exercise 5.4 - USER INPUT WITH LOOPS - Solution
+ * 
+ * Here is the solution in case you need assistance - give 5.3 an honest effort before looking at this solution.
+ * 
+ * Change the loop so that it counts backwards from the user input value until 1.
+ * 
+ */
+
+import java.util.Scanner; //Import the Scanner here
 
 public class D_UserUserInputWithLoopsSolution {
-
+    
+    public static void main(String[] args) {
+        //Create an object in the Scanner Class
+        Scanner myObj = new Scanner(System.in);    
+        //Prompt the user for a number
+        System.out.println("Enter a number:");
+        //Use Scanner to store the next Integer the user enters in a variable
+        int upperBound = myObj.nextInt();
+        for(int i = 1; i <= upperBound; i++){     //Note that we needed to use upperBound in the 'condition' part of the loop    
+            System.out.println("The value of i is: "+i);
+        }
+    }
 }
--- Lessons/src/Lessons_5/F_ConditionalStatementsExercise.java
+++ Lessons/src/Lessons_5/F_ConditionalStatementsExercise.java
@@ -1,5 +1,49 @@
 package Lessons_5;
+/* <----- Click this to Expand for Instructions
+ * 
+ * Exercise 5.6 - REVIEW - LOGIN SCREEN
+ * 
+ * 
+ * Task - Create a simple login screen that prompts the user for their Username and Password, and if they are 'correct' tell them they have 
+ * logged in successfully, and if not, tell them that they are incorrect and to try again. 
+ * 
+ * This exercise is possible with some clever use of a For loop, but it's much easier with a 'while' loop. This is a simpler type of loop 
+ * that does not have a counter. It can be controlled with any true/false condition (so think of making a boolean value to check if the 
+ * condition is correct).
+ * 
+ * HINT - Remember, String is not a primitive data type so you will need to use the .Equals() (See Exercise 3.7).
+ * 
+ * BONUS - have more than one valid username/password combination
+ * BONUS - if the user types Quit, have the program loop terminate (stop running).
+ * 
+ */
+
+import java.util.Scanner;
 
 public class F_ConditionalStatementsExercise {
-
+    
+    public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+        boolean loggedIn = false; 
+    	while(!loggedIn){
+    		System.out.println("Username: ");
+    		String user = input.nextLine();
+    		System.out.println("Password: ");
+    		String pass = input.nextLine();
+    		int length = pass.length();
+    		for(int i = 0; i < length; i++) {
+    		 System.out.print("*");	
+    		}
+    		if(user.equals("admin") && pass.equals("admin")) {
+    			System.out.println("\nAccess Granted");
+    			loggedIn = true;
+    		}else if (user.equals("Mr. P") && pass.equals("pong")) {
+    			System.out.println("\nAccess Granted");
+    			loggedIn = true;
+    		}else {
+    			System.out.print("\nUsername/Password are incorrect");
+    			System.out.println("\n");
+    		}
+    	}
+    }
 }
